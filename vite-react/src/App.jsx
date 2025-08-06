0e06b4a00f6f9df64c1541ae9de86879bc79b43a
GuSooBin <soobddg@gmail.com>
2025-08-06 20:14:05 +0900

    [React] 2일차 과제 제출

---
+++ vite-react/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Component } from 'react'
+
+// class App extends Component{
+//   render(){
+//     return <div>안녕하세요!</div>;
+//   }
+// }
+
+function App(){
+  return <div>함수형 컴포넌트입니다!</div>;
+}
+export default App
